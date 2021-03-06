89d65b6052cccee98b22aab293c66a8a40c7e59a
yfwang <wang.yifei6@husky.neu.edu>
2021-03-05 20:22:59 -0800

    test

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.connect(err => {
   app.post('/post-email', function (req, res) {
     
     collection.insertOne(req.body);
-    return res.redirect(req.get('referer'));
+    return res.redirect('back');
     //res.send('Data received:\n' + JSON.stringify(req.body));
 });
   //client.close();
