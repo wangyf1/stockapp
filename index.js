aa182b2bc71669cea9d276f8ad1fefcd36eb459e
yfwang <wang.yifei6@husky.neu.edu>
2021-04-03 20:29:24 -0700

    Refractor html to ejs

--- index.js
+++ index.js
@@ -9,19 +9,70 @@
 // server.listen(port, hostname, () => { 
 //  console.log(`Server running at http://${hostname}:${port}/`);
 // });
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 8080
-app.use(express.static(__dirname + '/client'));
+// const express = require('express')
+// const path = require('path')
+// const app = express()
+// const port = 8080
+// app.use(express.static(__dirname + '/client'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'index.html'));
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'index.ejs'));
+// })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+// Load Node modules
+var express = require('express');
+const router = express.Router();
+
+const ejs = require('ejs');
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static('public'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+// Port website will run on
+app.listen(8080);
+
+// *** GET Routes - display pages ***
+// Root Route
+app.get('/', function (req, res) {
+    res.render('index');
+});
+app.use("/css",express.static(__dirname + "/css"));
+app.use("/js",express.static(__dirname + "/js"));
+console.log('Example app listening at http://localhost:8080');
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/byd', (req, res) => {
+  res.render('byd');
+});
+app.get('/jdf', (req, res) => {
+  res.render('jdf');
+});
+app.get('/knowledge', (req, res) => {
+  res.render('knowledge');
+});
+app.get('/longi', (req, res) => {
+  res.render('longi');
+});
+app.get('/maotai', (req, res) => {
+  res.render('maotai');
+});
+app.get('/terms', (req, res) => {
+  res.render('terms');
+});
+app.get('/video', (req, res) => {
+  res.render('video');
+});
+app.get('/yonghui', (req, res) => {
+  res.render('yonghui');
+});
+
 var bodyParser = require('body-parser');
 
 app.use(bodyParser());
