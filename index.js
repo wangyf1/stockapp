ec9760e10a66421b4ce0c93c3f7be9e80d701c33
wangyf1 <33798237+wangyf1@users.noreply.github.com>
2021-04-06 23:54:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ app.get('/', function (req, res) {
 });
 app.use("/css", express.static(__dirname + "/css"));
 app.use("/js", express.static(__dirname + "/js"));
+app.use("/excel", express.static(__dirname + "/excel"));
+
 console.log('Example app listening at http://localhost:8080');
 app.get('/about', (req, res) => {
   res.render('about');
