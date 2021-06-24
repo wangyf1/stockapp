14af4c03ac161aff39591a715a26ea7a9bda5211
Williamburgson <guanjia.wang1994@gmail.com>
2021-06-24 17:17:39 -0400

    Push dev index to production

--- build.js
+++ build.js
@@ -160,8 +160,8 @@ function getData(code) {
     const indexData = await getIndex()
     console.log("Rendering index file")
     ejs2html({
-      path: `${__dirname}/views/dev_index.ejs`,
-      outPath: `${__dirname}/public/dev_index.html`,
+      path: `${__dirname}/views/index.ejs`,
+      outPath: `${__dirname}/public/index.html`,
       data: indexData,
     })
 
@@ -192,40 +192,14 @@ function getData(code) {
   }
 })()
 
-
-ejs2html({
-  path: `${__dirname}/views/index.ejs`,
-  outPath: `${__dirname}/public/index.html`
-});
-
 ejs2html({
   path: `${__dirname}/views/about.ejs`,
   outPath: `${__dirname}/public/about.html`
 });
 
 ejs2html({
-  path: `${__dirname}/views/byd.ejs`,
-  outPath: `${__dirname}/public/byd.html`
-});
-
-ejs2html({
-  path: `${__dirname}/views/jdf.ejs`,
-  outPath: `${__dirname}/public/jdf.html`
-});
-
-ejs2html({
   path: `${__dirname}/views/knowledge.ejs`,
   outPath: `${__dirname}/public/knowledge.html`
-});
-
-ejs2html({
-  path: `${__dirname}/views/longi.ejs`,
-  outPath: `${__dirname}/public/longi.html`
-});
-
-ejs2html({
-  path: `${__dirname}/views/maotai.ejs`,
-  outPath: `${__dirname}/public/maotai.html`
 });
 
 ejs2html({
@@ -239,21 +213,6 @@ ejs2html({
 });
 
 ejs2html({
-  path: `${__dirname}/views/yonghui.ejs`,
-  outPath: `${__dirname}/public/yonghui.html`
-});
-
-ejs2html({
-  path: `${__dirname}/views/pingan.ejs`,
-  outPath: `${__dirname}/public/pingan.html`
-});
-
-ejs2html({
-  path: `${__dirname}/views/nanfang.ejs`,
-  outPath: `${__dirname}/public/nanfang.html`
-});
-
-ejs2html({
   path: `${__dirname}/views/sidebar.ejs`,
   outPath: `${__dirname}/public/sidebar.html`
 });
@@ -261,44 +220,4 @@ ejs2html({
 ejs2html({
   path: `${__dirname}/views/contact.ejs`,
   outPath: `${__dirname}/public/contact.html`
-});
-
-ejs2html({
-  path: `${__dirname}/views/yiwei.ejs`,
-  outPath: `${__dirname}/public/yiwei.html`
-});
-
-ejs2html({
-  path: `${__dirname}/views/mairui.ejs`,
-  outPath: `${__dirname}/public/mairui.html`
-});
-
-ejs2html({
-  path: `${__dirname}/views/ziguang.ejs`,
-  outPath: `${__dirname}/public/ziguang.html`
-});
-
-ejs2html({
-  path: `${__dirname}/views/dfyh.ejs`,
-  outPath: `${__dirname}/public/dfyh.html`
-});
-ejs2html({
-  path: `${__dirname}/views/ningde.ejs`,
-  outPath: `${__dirname}/public/ningde.html`
-});
-ejs2html({
-  path: `${__dirname}/views/wuliangye.ejs`,
-  outPath: `${__dirname}/public/wuliangye.html`
-});
-ejs2html({
-  path: `${__dirname}/views/tianci.ejs`,
-  outPath: `${__dirname}/public/tianci.html`
-});
-ejs2html({
-  path: `${__dirname}/views/changchun.ejs`,
-  outPath: `${__dirname}/public/changchun.html`
-});
-ejs2html({
-  path: `${__dirname}/views/zhaoyi.ejs`,
-  outPath: `${__dirname}/public/zhaoyi.html`
 });
