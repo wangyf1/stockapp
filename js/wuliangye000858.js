684ba991c96772d89752e8a7d46e0608bd4bed5e
Qihang Ou <yidalidagege@Qihangs-MacBook-Pro.local>
2021-08-09 19:49:24 -0700

    newwuliangye

---
+++ js/wuliangye000858.js
@@ -0,0 +1,47 @@
+var ctx = loadChartCanvas('product')
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [ '2015-12-31', '2016-12-31', '2017-12-31', '2018-12-31',
+            '2019-12-31', '2020-12-31'],
+        datasets: [
+            {
+                label: "高价位酒",
+                backgroundColor: 'rgba(66, 135, 245, 1)',
+                borderColor: 'rgba(66, 135, 245,1)',
+                data: [155.07,174.16,219.94,301.89,386.76,440.6]
+            },
+            {
+                label: "中低价位酒",
+                backgroundColor: 'rgba(66, 245, 66, 1)',
+                borderColor: 'rgba(66, 245, 66, 1)',
+                data: [48.39,52.88,66.98,75.63,76.25,83.73]
+            },
+            
+            {
+                label: "塑料制品等",
+                backgroundColor: 'rgba(255, 156, 36, 1)',
+                borderColor: 'rgba(255, 156, 36, 1)',
+                data: [10.29,15.72,17.94,18.52,25.41,25.85]
+            }
+        ]
+    },
+    options: {
+        title: {
+            display: true,
+            text: '年营业收入（按不同产品划分）（亿元）'
+        },
+        responsive: false,
+        scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                },
+                stacked: true
+            }]
+        }
+    }
+});
